2e41885f69c897246b24fd70f4a599701081c918
Thomas Lamiable <86667484+Thomlam@users.noreply.github.com>
2024-10-24 14:14:58 +0200

    fix: fix crossbow system (#27)

--- Assets/Scripts/Trap/Arrow/Arrow.cs
+++ Assets/Scripts/Trap/Arrow/Arrow.cs
@@ -1,33 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float speed = 0.3f;  
-    public Vector3 direction = Vector3.right;  // Direction de d�placement de la fl�che
-    public float lifeTime = 5f;  // Dur�e de vie de la fl�che (si elle ne rencontre aucun obstacle)
+    public float speed = 0.3f;
+    public float lifeTime = 5f;
 
     void Start()
     {
-        
         Destroy(gameObject, lifeTime);
     }
 
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 
-    //TODO Fixe le syst�me pour les mur
     void OnCollisionEnter2D(Collision2D collision)
     {
-      
         if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
     }
-
 }
