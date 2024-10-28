6fadd9bf71dc4e6001fbc3fd5c32d400052f7098
Thomlam <lamiablethomas@gmail.com>
2024-10-28 02:18:38 +0100

    feat: add Interact mode for RPS, add "Totem" for launch that, add emit

---
+++ Assets/Scripts/Map Control/RoomSystem/EmitRPS.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIOClient;
+
+public class EmitRPS : InteractableObject
+{
+    public GameObject interactionCanvas;
+    public GameObject RPSCanvas;
+    private bool isPlayerNearby = false;    
+    private SocketIOUnity clientSocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clientSocket = SocketManager.Instance.ClientSocket;
+        interactionCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact(FindObjectOfType<Player>());  
+        }
+    }
+
+      public override void Interact(Player player)
+    {
+        Debug.Log("TAMEREss");
+        RPSCanvas.SetActive(true);
+        clientSocket.Emit("rps:start");
+    }
+
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactionCanvas.SetActive(true); 
+            isPlayerNearby = true;
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactionCanvas.SetActive(false);  
+            isPlayerNearby = false;
+           
+        }
+    }
+
+    
+    public override void SaveState(GameData gameData)
+    {
+       
+    }
+
+   
+    public override void RestoreState(GameData gameData)
+    {
+       
+    }
+    
+}
